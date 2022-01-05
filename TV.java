043206a88c07cfb1d067ebd3e2472b6e21d51c74
Kim yunseo <82978463+steam88ys@users.noreply.github.com>
2022-01-05 10:31:54 +0900

    TV 프로그램(클래스)

---
+++ TV.java
@@ -0,0 +1,87 @@
+package test.class1;
+
+public class TV {
+
+	private String color;
+	private int size;
+	private String producer;
+	private int channel, volume;
+	
+	public void setColor(String color) {
+		this.color = color;
+	}
+	
+	public String getColor() {
+		return color;
+	}
+
+	public int getSize() {
+		return size;
+	}
+
+	public void setSize(int size) {
+		this.size = size;
+	}
+
+	public String getProducer() {
+		return producer;
+	}
+
+	public void setProducer(String producer) {
+		this.producer = producer;
+	}
+
+	public int getChannel() {
+		return channel;
+	}
+
+	public void setChannel(int channel) {
+		this.channel = channel;
+	}
+
+	public int getVolume() {
+		return volume;
+	}
+
+	public void setVolume(int volume) {
+		this.volume = volume;
+	}
+	
+
+	public void powerOn() {
+		System.out.println(producer + " TV 전원이 켜짐");
+	}
+	
+	public void powerOff() {
+		System.out.println(producer + " TV 전원이 꺼짐");
+	}
+	
+	public void upChannel() {
+		channel++;
+		System.out.println("현재 채널은 " + channel + "번");
+	}
+	
+	public void downChannel() {
+		channel--;
+		System.out.println("현재 채널은 " + channel + "번");
+	}
+	
+	public void upVolume() {
+		volume++;
+		System.out.println("현재 볼륨은 " + volume);
+	}
+	
+	public void downVolume() {
+		volume--;
+		System.out.println("현재 볼륨은 " + volume);
+	}
+	
+	
+//	필드 값이 제대로 들어갔는지 확인 할 때
+	@Override
+	public String toString() {
+		return "TV [color=" + color + ", size=" + size + ", producer=" + producer + ", channel=" + channel + ", volume="
+				+ volume + "]";
+	}
+	
+}
