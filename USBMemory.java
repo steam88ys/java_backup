99aeeb3a104c8f97cd7aeb6225123c1d1f37e27b
Kim yunseo <82978463+steam88ys@users.noreply.github.com>
2022-01-06 10:47:20 +0900

    생성자2

---
+++ USBMemory.java
@@ -0,0 +1,40 @@
+package test.constructor2;
+
+import com.sun.security.auth.NTSidUserPrincipal;
+
+public class USBMemory {
+
+	private int capacity;
+	private String color;
+	
+	public int getCapacity() {
+		return capacity;
+	}
+	public String getColor() {
+		return color;
+	}
+	
+	public USBMemory(int capacity) {
+		this.capacity = capacity;
+	}
+	
+	public USBMemory(int capacity, String color) {
+		this(capacity);
+		this.color = color;
+	}
+	
+	public void showInfo() {
+		System.out.printf("%s색상의 %dGB인 USB 메모리", color, capacity);
+	}
+	
+	public void readUSBMemory() {
+		showInfo();
+		System.out.println("의 데이터를 읽는다.");
+	}
+	
+	public void writeUSBMemory() {
+		showInfo();
+		System.out.println("의 데이터를 저장한다.");
+	}
+	
+}
