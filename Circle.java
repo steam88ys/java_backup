0d1048b51e50387989c0bd7b1475290fb847fd92
Kim yunseo <82978463+steam88ys@users.noreply.github.com>
2022-01-07 10:43:53 +0900

    원의 둘레와 넓이(modifier)

--- Circle.java
+++ Circle.java
@@ -1,28 +1,38 @@
-package test.class3;
+package test.this2;
 
 public class Circle {
 
 	private int r;
 	private double circum, area;
 	
-	public int getR() {
-		return r;
-	}
-	public void setR(int r) {
-		this.r = r;
-	}
 	public double getCircum() {
 		return circum;
 	}
+
 	public double getArea() {
 		return area;
+	}
+
+	public int getR() {
+		return r;
+	}
+
+	public Circle(int r) {
+		this.r = r;
 	}
 	
 	public void calcCircum() {
 		circum = 2 * Math.PI * r;
 	}
 	
-	public void calcArea() {			// power : Á¦°ö -> Math.pow (Á¦°öÇÒ ¼ö, Á¦°ö È½¼ö)
+	public void calcArea() {
 		area = Math.PI * Math.pow(r, 2);
 	}
+	
+	public void showInfo() {
+		calcCircum();
+		calcArea();
+		RunCircle.showPrint(this);
+	}
+	
 }
