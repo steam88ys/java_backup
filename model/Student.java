7df19d2bd8fbee131aa5e5b7f4c6f131c8a9b113
Kim yunseo <82978463+steam88ys@users.noreply.github.com>
2022-01-13 11:51:13 +0900

    성적 프로그램 (모델링)

--- model/Student.java
+++ model/Student.java
@@ -1,5 +1,7 @@
 package mvc.score.model;
 
+// MVC Modeling Áß¿¡¼­ Data Model
+
 public class Student {
 
 	private String name;
--- view/ScoreView.java
+++ view/ScoreView.java
@@ -1,6 +1,7 @@
 package mvc.score.view;
 
 import java.util.ArrayList;
+// MVC Modeling 중에서 View(보여줌)에 해당하는 클래스: 출력
 import mvc.score.model.Student;
 
 public class ScoreView {
@@ -11,13 +12,27 @@ public class ScoreView {
 		this.studentArrList = studentArrList;
 	}
 	
-	public void printf() {
+	public void print() {
 		System.out.println("============= MVC 모델링을 기반으로 한 성적 프로그램 =============");
 		
 		for(int i= 0; i<studentArrList.size(); i++) {
 			Student s = studentArrList.get(i);
-			System.out.printf("* 성명: %s  학번: %d  국어: %d  영어: %d  수학:  %d\n");
+			int sum = calcSum(s);
+			double avg = calcAvg(sum);
+			System.out.printf("* 성명: %s  학번: %d  국어: %d  영어: %d  수학:  %d 과학:  %d 역사:  %d 합계:  %d 평균:  %.2f \n", 
+								s.getName(), s.getStuId(), s.getKor(), s.getEng(), s.getMath(), s.getScience(), s.getHistory(), 
+								sum, avg);
 		}
+	}
+	
+	public int calcSum (Student s) {	// 합계 메서드
+		int sum = s.getKor() + s.getEng() + s.getMath() + s.getScience() + s.getHistory();
+		return sum;
+	}
+	
+	public double calcAvg (int sum) {
+		double avg = sum / 5.0;
+		return avg;
 	}
 	
 }
