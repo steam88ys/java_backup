1aaa1d24d855e7a511ab8cf16f0c134ac06dc8af
Kim yunseo <82978463+steam88ys@users.noreply.github.com>
2022-04-06 11:35:53 +0900

    교과서 문제 풀어보기(상속, 76p 2-1예제)

---
+++ TextInheritance01/Leader.java
@@ -0,0 +1,13 @@
+package TextInheritance01;
+
+public class Leader extends Student{
+	
+	// 멤버변수
+	boolean boss;
+	
+	// 메소드
+	void bow() {
+		System.out.println("인사하다.");
+	}
+	
+}
---
+++ TextInheritance01/Student.java
@@ -0,0 +1,26 @@
+package TextInheritance01;
+
+public class Student {
+	
+	// 멤버변수 작성
+	String name;
+	String hakbun;
+	String telephone;
+	String address;
+	String department;
+	
+	// 메소드 작성
+	void study() {
+		System.out.println("공부하다.");
+	}
+	void eat() {
+		System.out.println("식사하다.");
+	}
+	void test() {
+		System.out.println("시험보다.");
+	}
+	void club() {
+		System.out.println("동아리 활동하다.");
+	}
+	
+}
