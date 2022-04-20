e2872375a50fb5a05ec2dcc7a8da220ebded7795
Kim yunseo <82978463+steam88ys@users.noreply.github.com>
2022-04-20 11:04:05 +0900

    Trim

---
+++ Trim.java
@@ -0,0 +1,20 @@
+package package01;
+
+public class Trim {
+
+	public static void main(String[] args) {
+		
+		String strData1 = "  I Love You  ";	// 문자열
+		String strData2;
+		strData2 = strData1.trim();
+		System.out.println("strData2.trim() ==>"+strData2);
+		// strData1이 가리키는 문자열에서 앞, 뒤의 공백을 제거하는 메소드
+		
+		if(strData1.equals(strData2))
+			System.out.println("같음");
+		else
+			System.out.println("다름");
+		
+	}
+
+}
