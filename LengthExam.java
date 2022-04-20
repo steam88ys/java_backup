2d4af760381fee4f8909e4c8a23a6cd026802aef
Kim yunseo <82978463+steam88ys@users.noreply.github.com>
2022-04-20 10:43:18 +0900

    Length

---
+++ LengthExam.java
@@ -0,0 +1,11 @@
+package package01;
+
+public class LengthExam {
+
+	public static void main (String[] args) {
+		String strData1 = "I Love You";
+		System.out.println("strData1.length() = " + strData1.length());
+		// strData1이 가리키는 문자열의 갯수(공백도 포함)
+	}
+	
+}
