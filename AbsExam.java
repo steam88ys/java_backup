d800679b858c3b603b1b23289c03403714b8c732
Kim yunseo <82978463+steam88ys@users.noreply.github.com>
2022-04-27 10:37:30 +0900

    Mth.abs (절대값)

---
+++ AbsExam.java
@@ -0,0 +1,15 @@
+package package04;
+
+public class AvsExam {
+
+	public static void main(String[] args) {
+
+		// abs는 absolute(절대값) --> 무조건 양수가 출력
+		System.out.println(Math.abs(25));
+		System.out.println(Math.abs(-25));
+		System.out.println(Math.abs(25.5));
+		System.out.println(Math.abs(-25.5));
+		
+	}
+
+}
