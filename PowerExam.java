4cd555180813dce9132b5ab4a027a49303490f64
Kim yunseo <82978463+steam88ys@users.noreply.github.com>
2022-04-27 10:43:38 +0900

    Math.pow (거듭제곱)

---
+++ PowerExam.java
@@ -0,0 +1,15 @@
+package package04;
+
+public class PowerExam {
+
+	public static void main(String[] args) {
+
+		System.out.println(Math.pow(3, 2));	// 3ÀÇ 2Á¦°ö 9.0
+		System.out.println(Math.pow(3.0f, 2.0f));
+		System.out.println(Math.pow(5.0, 2));
+		System.out.println(Math.pow(-5.0, 3.0));
+		// °ÅµìÁ¦°öÀ» ±¸ÇÏ´Â ¸Å¼Òµå -5ÀÇ 3.0Á¦°ö -125
+		
+	}
+
+}
