001bd4385dec7a8c1c80dc094e826fc8d784a88a
Kim yunseo <82978463+steam88ys@users.noreply.github.com>
2022-04-27 10:46:18 +0900

    Math.sqrt (제곱근 반환)

---
+++ SquareExam.java
@@ -0,0 +1,15 @@
+package package04;
+
+public class SquareExam {
+
+	public static void main(String[] args) {
+
+		System.out.println(Math.sqrt(9));
+		System.out.println(Math.sqrt(9.0f));
+		System.out.println(Math.sqrt(9.0));
+		System.out.println(Math.sqrt(-9.0));
+		// sqrt는 square root의 약자
+		
+	}
+
+}
