b867bfd95813df0f5bb44a57ee29fdf7e7a549b1
Kim yunseo <82978463+steam88ys@users.noreply.github.com>
2022-08-16 16:10:59 +0900

    멀티스레드 (Runnable 인터페이스 사용)

--- Top.java
+++ Top.java
@@ -1,8 +1,9 @@
-package thread01;
+package thread03;
 
-public class Top extends Thread{	// Thread 클래스를 상속 받아야 한다.
+public class Top extends Thread{	
+	// DerivedThread는 Thread 클래스를 반드시 상속 받아야 한다.
 	public void run() {
-		for(int i=0; i<50; i++) {
+		for(int i=0; i<20; i++) {
 			System.out.print(i + "\t");
 		}
 	}
