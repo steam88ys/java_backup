febf4443e8a85bb6d0c19194ca7d84a5bdcd0046
Kim yunseo <82978463+steam88ys@users.noreply.github.com>
2022-08-17 10:53:39 +0900

    Runnable 수정

--- Runnable1Exam.java
+++ Runnable1Exam.java
@@ -5,9 +5,9 @@ public class Runnable1Exam {
 	public static void main(String[] args) {
 		System.out.println("프로그램 시작");
 		
-		Top t = new Top();
-		Thread thd = new Thread(t);
-		// Thread thd = new Thread( new Top() );
+		// 1.Top t = new Top();
+		// 2. Thread thd = new Thread(t);
+		Thread thd = new Thread( new Top() );	// 위의 1,2 표현을 한문장으로 표현
 		
 		thd.start();
 		System.out.println("프로그램 종료");
--- Top.java
+++ Top.java
@@ -1,6 +1,6 @@
 package thread03;
 
-public class Top extends Thread{	
+public class Top implements Runnable{	
 	// DerivedThread는 Thread 클래스를 반드시 상속 받아야 한다.
 	public void run() {
 		for(int i=0; i<20; i++) {
