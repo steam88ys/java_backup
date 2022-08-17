e657c68f983e9de9605820029be565370b29a310
Kim yunseo <82978463+steam88ys@users.noreply.github.com>
2022-08-17 12:10:56 +0900

    priority

--- Top.java
+++ Top.java
@@ -1,11 +1,9 @@
-package thread03;
+package thread05;
 
-public class Top implements Runnable{	
-	// DerivedThread는 Thread 클래스를 반드시 상속 받아야 한다.
+public class Top implements Runnable{
 	public void run() {
-		for(int i=0; i<20; i++) {
-			System.out.print(i + "\t");
+		for(int i=0; i<30; i++) {
+			System.out.print(Thread.currentThread().getName()+i+"\t");
 		}
 	}
-
 }
